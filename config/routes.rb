747257e92ac4d79082c69e7d386009e3cc72e43e
Toshiaki Genda <prnd@team.email.ne.jp>
2019-09-15 16:06:17 +0000

    Finish top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'static_pages#top'     # topページはこのアプリケーションの初期ページとなるのでroot設定にする
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
