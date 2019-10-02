128cd1e4f37cc136849e6d8155cfc2161cefb078
Toshiaki Genda <prnd@team.email.ne.jp>
2019-10-02 22:09:48 +0000

    6.2 login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
   root 'static_pages#top'     # topページはこのアプリケーションの初期ページとなるのでroot設定にする
   get '/signup', to: 'users#new'
+  
+  #ログイン機能
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy' 
+  
   resources :users
 end
